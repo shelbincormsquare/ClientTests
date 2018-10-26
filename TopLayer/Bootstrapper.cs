48145313175e61767e0c27779e43850821c8075c
shelbincormsquare <shelbin@cormsquare.com>
2018-10-26 09:54:23 +0530

    Add project files.

---
+++ TopLayer/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Caliburn.Micro;
+using System.Windows;
+using TopLayer.ViewModels;
+
+namespace TopLayer
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
